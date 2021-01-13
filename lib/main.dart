6014a7c7e3b35e7d85d32fee453d01148a5387d2
Nisarg Mangukiya <mangukianisarg@gmail.com>
2021-01-13 18:57:05 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/provider/great_places.dart';
+import 'package:great_places/screens/add_place_screen.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() =>
-    runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
-      ),
-      home:,
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Great Places',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          }),
     );
   }
 }
